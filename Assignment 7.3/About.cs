6bcaf3e9949f077753b9284677b05be830362912
zmil44 <zachary.j.miller@outlook.com>
2018-08-14 12:36:47 -0400

    initial commit

---
+++ Assignment 7.3/About.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+/*Zachary Miller
+ * 12/20/2017
+ * assignment 7.3
+ * The purpose of this program is display a form with an about button that will display a copyright message when clicked
+ * InternetAccess.cs
+ * Bellevue University
+ */
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_7._3
+{
+    static class About
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AboutForm());
+        }
+    }
+}
---
+++ Assignment 7.3/AboutForm.cs
@@ -0,0 +1,35 @@
+﻿/*Zachary Miller
+ * 12/20/2017
+ * assignment 7.3
+ * This contains the code for the form and button 
+ * InternetAccess.cs
+ * Bellevue University
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_7._3
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const String copyright = ("Copyright December 2017\n Zachary Miller\n Bellevue University\n ");
+            const String caption = ("CopyRight");
+            System.Windows.Forms.MessageBox.Show(copyright, caption, MessageBoxButtons.OK);
+        }
+    }
+}
